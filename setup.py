abb09f17fe8fc01ed26d86d3f0ce6d5e643aae72
Jason Green <JayTeeGeezy@outlook.com>
2016-04-19 09:56:37 +1000

    Implement minimal structure

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+
+setup(
+	name='pypunters',
+	version='0.0.0',
+	description='Python web scraper for www.punters.com.au',
+	url='https://github.com/JayTeeGeezy/pypunters',
+	author='Jason Green',
+	author_email='JayTeeGeezy@outlook.com',
+	license='MIT',
+	packages=[
+		'pypunters'
+	],
+	zip_safe=False
+	)
